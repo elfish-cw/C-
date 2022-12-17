1056c1b8eb968fe2d77a70f55efa190901766b34
Lana <elfish_cw@mail.ru>
2022-12-17 21:53:36 +0300

    задание 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        Console.WriteLine("введите 2 числа");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2) 
+        {
+            Console.WriteLine($"Число {num1} большее, число {num2} меньшее");
+        }
+else
+if (num1 < num2) 
+        {
+            Console.WriteLine($"Число {num2} большее, число {num1} меньшее");
+        }
+else
+            Console.WriteLine("Числа равны");
+
