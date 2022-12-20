1c83ebe98b0e895c280537eb25fdde49eb1dfae6
Lana <elfish_cw@mail.ru>
2022-12-20 16:46:19 +0300

    сделала задание 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+} 
+else 
+Console.WriteLine($"{number} - нечетное число");
