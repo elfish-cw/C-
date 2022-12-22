76e5750a8f23040bf2c5f1d7248169cf56e0b0e6
Lana <elfish_cw@mail.ru>
2022-12-22 16:20:36 +0300

    сделала задание 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("введите положительное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine ("нет четных чисел в диапозоне до введенного числа");
+    }
+while (number > 1)
+{
+    {if (number % 2 == 0)
+
+    {Console.WriteLine($"{number}");}}
+
+    number = (number - 1);
+}
+
