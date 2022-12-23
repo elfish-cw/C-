d132fd03ab99de8febee4917f567d677f36788ad
Lana <elfish_cw@mail.ru>
2022-12-23 13:10:28 +0300

    решила задачу 10

--- задача 10/Program.cs
+++ задача 10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимаем на вход трехзначное число и на выходе показываем вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int second = SecondDigit(number);
+    Console.WriteLine ($"вторая цифра этого числа = {second}");
+    
+
+}
+else Console.WriteLine ("Вы ввели не трехзначное число");
+
+int SecondDigit(int num)
+{
+    int secondDigit = num / 10 % 10; 
+    return secondDigit;
+}
+
