c504262f1c377113c137f4c6ab4af376f78a7a2b
Lana <elfish_cw@mail.ru>
2022-12-27 16:34:23 +0300

    решила задание 13

--- задача 13/Program.cs
+++ задача 13/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число, в котором мы будем искать третью цифру :");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+      number = number * (-1);
+      }
+if (number > 99)
+{
+      while (number / 10 > 99)
+    
+{
+      number = number / 10;
+}
+int thirdDigit = number % 10;
+Console.WriteLine($"третья цифра слева числа = {thirdDigit}");
+
+}
+
+else Console.WriteLine ("данное число не имеет третьей цифры");
+
