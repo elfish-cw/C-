4f81ccd8f2ead6fa57cb80be06149b6e0d0676b0
Lana <elfish_cw@mail.ru>
2022-12-28 12:51:00 +0300

    решила задачу 23

--- задача 23/Program.cs
+++ задача 23/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= number)
+{Console.WriteLine($"{index} | {index * index * index}");
+index++;
+}
