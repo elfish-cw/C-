72225d195d45d21ba8a468328b00fd14cb577ae7
Lana <elfish_cw@mail.ru>
2022-12-28 11:05:12 +0300

    решила задание 19

--- задача 19/Program.cs
+++ задача 19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100000 && number > 9999)
+{
+    if (number % 10 == number / 10000 && number / 10 % 10 == number / 1000 % 10)
+        Console.WriteLine("число палиндром");
+    else
+    {
+        Console.WriteLine("число НЕ палиндром");
+    }
+
+}
+else
+{
+    Console.WriteLine("ввели неправильное число");
+}
