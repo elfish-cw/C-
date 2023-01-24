87e972d4f1118222f5ea8adf9a91fe2ce83bb015
Lana <elfish_cw@mail.ru>
2023-01-24 17:04:47 +0300

    решила задачу 24

--- задача 24/Program.cs
+++ задача 24/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача 24. Напишите программу, которая принимает
+// на вход число (А) и выдает сумму чисел от 1 до А.
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// int sumnumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {SumNumbers(number)}");
+int SumNumbers (int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= num; i++)
+//     {
+        
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num;
+        num -= 1;
+    }
+    return sum;
+}
+
