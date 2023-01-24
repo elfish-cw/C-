f0f3391ed32769bc11e3ff16def984b80e374d78
Lana <elfish_cw@mail.ru>
2023-01-24 12:10:05 +0300

    решила задачу 26

---
+++ задача 26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдает количество 
+// цифр в числе
+// 456 ->3
+
+
+int InsertDigit(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+int CountDigit(int digit)
+{
+    digit = Math.Abs(digit);
+    int sum = 0;
+    while (digit > 0)
+    {
+        digit = digit / 10;
+        sum++;
+    }
+    return sum;
+}
+
+int num = InsertDigit("введите число");
+Console.WriteLine($"количество цифр в числе {num} = {CountDigit(num)}"); 
