fba0eba0445effd5700f71821ed8b87f365f266f
Lana <elfish_cw@mail.ru>
2023-01-24 14:11:08 +0300

    решила задание 27

--- задача 27/Program.cs
+++ задача 27/Program.cs
@@ -2,3 +2,25 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int InsertDigit(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumDigits (int insertNum)
+{
+    insertNum = Math.Abs(insertNum);
+    int sum = 0;
+ while (insertNum>0)
+ {
+ sum = sum+insertNum%10;
+ insertNum = insertNum/10;
+ }
+ return sum;
+}
+int num = InsertDigit("введите число:");
+Console.WriteLine($"в числе {num} сумма цифр = {SumDigits(num)}");
+
