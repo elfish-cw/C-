3dc465e57725f032fde42ea5f9627286ddfce1e9
Lana <elfish_cw@mail.ru>
2023-01-24 13:27:21 +0300

    решила задачу 30

--- задача 30/Program.cs
+++ задача 30/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Задача 30. Написать программу, которая выводит массив из 8 элементов, 
 // заполненный случайными числами единицами и нулями.
 
+int [] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,2);
+}
+for (int i = 0; i < array.Length; i++)
+{ Console.Write(array[i]);
+}
+Console.WriteLine();
