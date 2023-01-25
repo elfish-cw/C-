9c4f4743a6be9b90b2491e3e5118ad8423fdb482
Lana <elfish_cw@mail.ru>
2023-01-25 14:27:05 +0300

    решила задачу 39

--- задача 39/Program.cs
+++ задача 39/Program.cs
@@ -3,22 +3,54 @@
 // // месте, а первый - на последнем и т.д.)
 // // [1 2 3 4 5] -> [5 4 3 2 1]
 // // [6 7 3 6] -> [6 3 7 6];
-void ReverseArray(int[] arr)
+
+int[] CreateArrayRndInt(int size, int min, int max)
 {
-    for (int i = 0; i < arr.Length / 2; i++)
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
     {
-        int temp = arr[i];
-        arr[i] = arr[arr.Length - 1 - i];
-        arr[arr.Length - 1 - i] = temp;
+        array[i] = rnd.Next(min, max + 1);
     }
+    return array;
 }
 
-int[] array = CreateArrayRndInt(5, 1, 10);
-PrintArray(array);
-ReverseArray(array);
-PrintArray(array);
-Array.Reverse(array);
-PrintArray(array);
+void PrintArray(int[] array1)
+{
+    Console.Write("[");
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (i < array1.Length - 1) Console.Write($"{array1[i]}, ");
+        else Console.Write($"{array1[i]}]");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] array2)
+{
+    int temp = 0;
+    for (int i = 0; i < array2.Length / 2; i++)
+    {
+        temp = array2[i];
+        array2[i] = array2[array2.Length-1-i];
+        array2[array2.Length-1-i]=temp;
+    }
+    }
+
+
+
+int[] arr = CreateArrayRndInt(5, 0, 9);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
+// Array.Reverse(arr);
+// PrintArray(arr);
+
+
+
+
+
+
 
 
 
