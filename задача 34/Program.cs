38dce8d7836b75106423e4e5dd179d7f3cd6de5a
Lana <elfish_cw@mail.ru>
2023-01-25 14:00:23 +0300

    отредактировала

--- задача 34/Program.cs
+++ задача 34/Program.cs
@@ -15,35 +15,37 @@
 
 int[] CreateArrayRndInt(int size, int min, int max)
 {
-Random rnd = new Random();
-int [] array = new int [size];
-for (int i = 0; i < array.Length; i++)
-{
-    array[i]=rnd.Next(min, max+1);
-}
-return array;
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
 }
 
-void PrintArray (int [] array1)
+void PrintArray(int[] array1)
 {
     Console.Write("[");
     for (int i = 0; i < array1.Length; i++)
-    {if (i<array1.Length-1) Console.Write($"{array1[i]}, ");
-    else Console.Write($"{array1[i]}]");   
+    {
+        if (i < array1.Length - 1) Console.Write($"{array1[i]}, ");
+        else Console.Write($"{array1[i]}]");
     }
-Console.WriteLine();
+    Console.WriteLine();
 }
 
-int CountEvenNum(int [] array2 )
+int CountEvenNum(int[] array2)
 {
     int sum = 0;
     for (int i = 0; i < array2.Length; i++)
-    {if (array2[i]%2==0) sum++; 
-        } 
+    {
+        if (array2[i] % 2 == 0) sum++;
+    }
     return sum;
-} 
+}
 
-int [] arr = CreateArrayRndInt(6,99,999);
+int[] arr = CreateArrayRndInt(6, 99, 999);
 PrintArray(arr);
 Console.WriteLine($"количество четных чисел в массиве = {CountEvenNum(arr)}");
 
