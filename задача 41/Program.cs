5f4d9c1777aa470d38d3788e929bc7630991da73
Lana <elfish_cw@mail.ru>
2023-01-25 23:47:58 +0300

    создала задание 41

--- задача 41/Program.cs
+++ задача 41/Program.cs
@@ -1,42 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
 
-int ParseIn2(int num)
-{
-    if (num == 0) return 0;
-    int result = num % 2;
-    num /= 2;
-    result += ParseIn2(num) * 10;
-    return result;
-}
-//Console.WriteLine(ParseIn2(Convert.ToInt32(Console.ReadLine())));
-Console.WriteLine("Введите чило");
-var num =Console.ReadLine();
-Console.WriteLine(ParseIn2(Convert.ToInt32(num)));
-
-
-// / Задача 42: Напишите программу, которая будет преобразовывать
-// // десятичное число в двоичное.
-// // 45 -> 101101
-// // 3 -> 11
-// // 2 -> 10
-
-// int number = NumberInput("Please, input your number");
-// Console.WriteLine(DecToBin(number));
-
-// int NumberInput(string text) // Method fot Number input
-// {
-//     Console.WriteLine(text);
-//     int numberinput = Convert.ToInt32(Console.ReadLine());
-//     return numberinput;
-// }
-
-// string DecToBin(int num)
-// {
-//     string result = string.Empty;
-//     while (num > 0)
-//     {
-//         result = num % 2 + result;
-//         num /= 2;
-//     }
-//     return result;
-// }
