efb6c2cf022392fc826a1f2e3fd4a9306b6c2150
Lana <elfish_cw@mail.ru>
2023-01-25 23:24:15 +0300

    решила задачу 44

--- задача 44/Program.cs
+++ задача 44/Program.cs
@@ -1,46 +1,12 @@
-﻿int[] CreateArrayRndInt(int size, int min, int max)
-{
-    int[] array = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(min, max + 1);
-    }
-    return array;
-}
+﻿// Задача 44. Не используя рекурсию, выведите первые N
+// чисел Фибоначчи. Первые 2 числа Фибоначчи: 0,1.
+// N=5 ->0 1 1 2 3
+// N=3 ->0 1 1 
+// N=7 ->0 1 1 2 3 5 8
 
 
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(i < array.Length - 1) Console.Write (array[i] + ", ");
-        else Console.Write (array[i]);
-    }
-    Console.WriteLine("]");
-}
 
-int[] CopyArray(int[] array)
-{
-    int[] newArray = new int[array.Length];
-    for (int i = 0; i < newArray.Length; i++)
-    {
-        newArray[i] = array[i];
-    } 
-    return newArray;
-}
 
-int[] arr = CreateArrayRndInt(4, 0, 10);
-PrintArray(arr);
-int[] newArr = CopyArray(arr);
-PrintArray(newArr);
-int num = InsertPos("Введите число");
-int InsertPos(string text)
-{
-Console.WriteLine(text);
-return Convert.ToInt32(Console.ReadLine());
-}
 int[] Fibonacci(int num)
 {
     int[] fibo = new int[num];
@@ -63,5 +29,13 @@ void PrintArray(int[] array)
     }
     Console.WriteLine("]");
 }
-int[] fiboArray = Fibonacci(num);
+
+int InputNum (string text) // Method fot Number input
+{
+    Console.WriteLine(text);
+    int numberinput = Convert.ToInt32(Console.ReadLine());
+    return numberinput;
+}
+int number = InputNum("введите N");
+int[] fiboArray = Fibonacci(number);
 PrintArray(fiboArray);
