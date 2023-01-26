72766b3f23ccbe123d0a1ae4cd00f654a16771e0
Lana <elfish_cw@mail.ru>
2023-01-26 14:40:09 +0300

    решила задачу 49

--- задача 49/Program.cs
+++ задача 49/Program.cs
@@ -3,18 +3,47 @@
 // квадраты.
 // Например, изначально массив
 // выглядел вот так:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+// 1 4 7 2      1 4 49 2
+// 5 9 2 3      5 9  2 3
+// 8 4 2 4     64 4  4 4 
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix1)
+{
+  
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+          Console.Write("[");
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            if (j < matrix1.GetLength(1) - 1) Console.Write($"{matrix1[i, j],5},");
+            else Console.Write($"{matrix1[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
 
 
 void SquareMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
         {
-            matrix[i,j]*=matrix[i,j];
+            matrix[i, j] *= matrix[i, j];
         }
     }
 }
