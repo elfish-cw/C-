428dc56a1aac92b91ed9e314fe62c88379a22ad4
Lana <elfish_cw@mail.ru>
2023-01-26 14:58:10 +0300

    решила задание 51

--- задача 51/Program.cs
+++ задача 51/Program.cs
@@ -8,10 +8,6 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
 
-int[,] arr1 = CreateMatrixRndInt(3, 4, 1, 50);
-PrintMatrix(arr1);
-Console.Write(MatrixTrace(arr1));
-
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
@@ -26,19 +22,6 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     return matrix;
 }
 
-// Суммирование элементов на диагонали
-int MatrixTrace(int[,] matrix)
-{
-    int sum = 0;
-    int k = 0;
-    if (matrix.GetLength(0) < matrix.GetLength(1)) k = matrix.GetLength(0);
-    else k = matrix.GetLength(1);
-    for (int i = 0; i < k; i++)
-    {
-        sum += matrix[i, i];
-    }
-    return sum;
-}
 
 // Печать массива int
 void PrintMatrix(int[,] matrix)
@@ -49,9 +32,45 @@ void PrintMatrix(int[,] matrix)
         Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
-            else Console.Write($"{matrix[i, j],4}");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5} ");
         }
         Console.WriteLine("]");
     }
 }
+
+// Суммирование элементов на диагонали
+int MatrixTrace(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += i==j ? matrix[i,j]: 0; 
+        }
+    }
+    return sum;
+}
+
+
+
+
+
+// Суммирование элементов на диагонали
+// int MatrixTrace(int[,] matrix)
+// {
+//     int sum = 0;
+//     int k = 0;
+//     if (matrix.GetLength(0) < matrix.GetLength(1)) k = matrix.GetLength(0);
+//     else k = matrix.GetLength(1);
+//     for (int i = 0; i < k; i++)
+//     {
+//         sum += matrix[i, i];
+//     }
+//     return sum;
+// }
+
+int[,] arr1 = CreateMatrixRndInt(5, 5, 1, 50);
+PrintMatrix(arr1);
+Console.WriteLine(MatrixTrace(arr1));
