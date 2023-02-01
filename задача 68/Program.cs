d282fbb920163d2de14bf7dd29f7b1af162e0a0b
Lana <elfish_cw@mail.ru>
2023-02-01 13:21:14 +0300

    редактировала

--- задача 68/Program.cs
+++ задача 68/Program.cs
@@ -6,19 +6,19 @@
 
 Console.WriteLine("Введите натуральное неотрицательное число A(меньше 4)");
 int numberA = Convert.ToInt32(Console.ReadLine());
-if (numberA < 0 || numberA>4) Console.WriteLine("Ошибка ввода. Введите натуральное неотрицательное число A(меньше 4)");
+if (numberA < 0 || numberA > 4) Console.WriteLine("Ошибка ввода. Введите натуральное неотрицательное число A(меньше 4)");
 
 Console.WriteLine("Введите натураьное число B");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberB < 0 || numberB>4) 
-Console.WriteLine("Ошибка ввода. Введите натуральное неотрицательное число В(меньше 4)");
+if (numberB < 0 || numberB > 4)
+    Console.WriteLine("Ошибка ввода. Введите натуральное неотрицательное число В(меньше 4)");
 
 
 Console.WriteLine(Akkerman(numberA, numberB));
 
 int Akkerman(int numA, int numB)
 {
-    if (numA == 0) return numB+1;
-    else if (numB == 0) return Akkerman(numA-1,1);
-    return Akkerman(numA-1, Akkerman(numA, numB-1));
+    if (numA == 0) return numB + 1;
+    else if (numB == 0) return Akkerman(numA - 1, 1);
+    return Akkerman(numA - 1, Akkerman(numA, numB - 1));
 }
